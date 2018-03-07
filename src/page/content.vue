b4f15e1d604a447e47aaed6e881a16e43300ed7c
BeckHelloWord! <yuwei_shiny@163.com>
2018-03-07 22:10:59 +0800

    内容页

--- src/page/content.vue
+++ src/page/content.vue
@@ -1,26 +1,40 @@
 <template>
-    <div>
-
-    </div>
+  <div>
+    <myHeader></myHeader>
+    <h2 v-text="dat.title"></h2>
+    <p>作者：{{dat.author?dat.author.loginname:''}}　　发表于：{{$utils.goodTime(dat.create_at)}}</p>
+    <hr>
+    <article v-html="dat.content"></article>
+    <h3>网友回复：</h3>
+    <ul>
+      <li v-for="i in dat.replies" :key="i.id">
+        <p>评论者：{{i.author.loginname}}　　评论于：{{$utils.goodTime(i.create_at)}}</p>
+        <article v-html="i.content"></article>
+      </li>
+    </ul>
+    <myFooter></myFooter>
+  </div>
 </template>
 <script>
+import myHeader from "../components/header";
+import myFooter from "../components/footer";
 export default {
+  components: { myFooter, myHeader },
   data() {
     return {
-      lists: []
+      id: this.$router.history.current.params.id,
+      dat: {}
     };
   },
-  created() {
-    var id = this.$router.history.current.params.id;
-    this.get_data({ id: id });
+  mounted() {
+    this.$nextTick(function() {
+      this.getData();
+    });
   },
   methods: {
-    get_data: function(params) {
-      var _this = this;
-      if (!params) params = {};
-      // 我们这里用全局绑定的 $api 方法来获取数据，方便吧~
-      _this.$api.get("topics", params, function(r) {
-        _this.lists = r.data;
+    getData() {
+      this.$api.get("topic/" + this.id, null, r => {
+        this.dat = r.data;
       });
     }
   }
