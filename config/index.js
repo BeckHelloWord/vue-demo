e766343535c5a14ff81d0b027417a80fe24a2e1c
BeckHelloWord! <444870506@qq.com>
2018-03-08 17:06:40 +0800

    打包路径设置

--- config/index.js
+++ config/index.js
@@ -56,13 +56,13 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/m/dist/', //服务器下希望放在什么路径下，就写什么路径
 
     /**
      * Source Maps
      */
-
-    productionSourceMap: true,
+    //不编译生成.map文件
+    productionSourceMap: false,
     // https://webpack.js.org/configuration/devtool/#production
     devtool: '#source-map',
 
